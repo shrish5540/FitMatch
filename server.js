46c05cc09bca376108be87cf105a495925d86b08
Shrishti Gupta <guptashrishti5540@gmail.com>
2026-05-28 23:00:24 +0530

    fix cors

--- server.js
+++ server.js
@@ -20,8 +20,10 @@ const server = http.createServer(app)
 const io = new Server(server, {
   cors: { origin: '*' }
 })
-
-app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://fitmatch.vercel.app'],
+  credentials: true
+}))
 app.use(express.json())
 
 app.use('/api/users', userRoutes)
